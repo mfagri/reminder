ea419458c54834fc2faf66181279a666e70738e1
marouane.fagri1 <marouane.fagri1@gmail.com>
2024-08-20 16:56:13 +0000

    add new scan package

--- lib/view/friends/add_friends.dart
+++ lib/view/friends/add_friends.dart
@@ -3,7 +3,7 @@ import 'package:myapp/helpers/icon_constants.dart';
 import 'package:myapp/helpers/utils.dart';
 import 'package:myapp/view/friends/friends_item.dart';
 // import 'package:simple_barcode_scanner/simple_barcode_scanner.dart';
-import 'package:flutter_qr_scan/flutter_qr_scan.dart';
+import 'package:scan/scan.dart';
 
 class AddFriends extends StatefulWidget {
   const AddFriends({super.key});
@@ -13,13 +13,11 @@ class AddFriends extends StatefulWidget {
 }
 
 class _AddFriendsState extends State<AddFriends> {
-  QrReaderViewController controller = QrReaderViewController(1);
-  late String data;
-  late String rawData;
   @override
   void initState() {
     super.initState();
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -96,19 +94,12 @@ class _AddFriendsState extends State<AddFriends> {
                   contentPadding: const EdgeInsets.symmetric(horizontal: 10),
                   suffixIcon: InkWell(
                     onTap: () async {
-                      await controller.startCamera(onScan);
-                      // Navigator.pushNamed(context, '/qr');
-                      // var res = await Navigator.push(
-                      //     context,
-                      //     MaterialPageRoute(
-                      //       builder: (context) =>
-                      //           const SimpleBarcodeScannerPage(),
-                      //     ));
-                      // setState(() {
-                      //   if (res is String) {
-                      //     //print(res);
-                      //   }
-                      // });
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => const ScanViwePage(),
+                        ),
+                      );
                     },
                     child: const Row(
                       children: [
@@ -147,13 +138,95 @@ class _AddFriendsState extends State<AddFriends> {
       ),
     );
   }
+}
 
-  void onScan(String? v, List<Offset> offsets, String? raw) {
-    print([v, offsets, raw]);
-    setState(() {
-      data = v!;
-      rawData = raw!;
-    });
-    controller.stopCamera();
+//scan view
+class ScanViwePage extends StatefulWidget {
+  const ScanViwePage({super.key});
+
+  @override
+  State<ScanViwePage> createState() => _ScanViwePageState();
+}
+
+class _ScanViwePageState extends State<ScanViwePage> {
+  ScanController controller = ScanController();
+  String qrcode = 'Unknown';
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xffF2F2F2),
+      appBar: AppBar(
+        forceMaterialTransparency: true,
+        scrolledUnderElevation: 0,
+        leading: Container(
+          height: 100,
+          color: const Color(0xffE3E3E3),
+          child: Center(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              child: Row(
+                children: [
+                  const Text(
+                    'Scan QR Code',
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 16,
+                      fontFamily: 'Roboto',
+                      fontWeight: FontWeight.w500,
+                      height: 0.06,
+                    ),
+                  ),
+                  const Spacer(),
+                  InkWell(
+                      onTap: () {
+                        Navigator.pop(context);
+                      },
+                      child:
+                          appIcon(IconsConstants.back, false, context, 15, 15))
+                ],
+              ),
+            ),
+          ),
+        ),
+        leadingWidth: MediaQuery.of(context).size.width,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            const Text(
+              'Scan for added friend',
+              style: TextStyle(
+                color: Colors.black,
+                fontSize: 16,
+                fontFamily: 'Roboto',
+                fontWeight: FontWeight.w500,
+              ),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            SizedBox(
+              width: 250, // custom wrap size
+              height: 250,
+              child: ScanView(
+                controller: controller,
+                // custom scan area, if set to 1.0, will scan full area
+                scanAreaScale: .7,
+                scanLineColor: const Color(0xff1488CC),
+                onCapture: (data) {
+                  print(data);
+                  setState(() {
+                    qrcode = data;
+                  });
+                  // Navigator.pop(context);
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
